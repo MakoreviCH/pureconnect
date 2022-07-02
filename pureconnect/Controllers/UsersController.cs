fcf21ff9c4ed19a129a5167f99af793eaa5b04e8
rosti <rosti@DESKTOP-5QV4CJA>
2022-07-02 16:12:24 +0300

    Add project files.

---
+++ pureconnect/Controllers/UsersController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace pureconnect.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IConfiguration Configuration;
+        public UsersController(IConfiguration _configuration)
+        {
+            Configuration = _configuration;
+        }
+        [HttpGet]
+        [Route("api/[controller]")]
+        public List<User> GetUser()
+        {
+            string query = "SELECT * FROM Users";
+            string connectionString = Configuration.GetConnectionString("PureDatabase");
+            var jsonResult = new StringBuilder();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                List<User> users = new List<User>();
+                while (reader.Read())
+                {
+                    User u = new User();
+                    u.ID = reader.GetValue(0).ToString();
+                    u.First_Name=reader.GetValue(1).ToString();
+                    u.Last_Name=reader.GetValue(2).ToString();
+                    u.Username=reader.GetValue(3).ToString();
+                    u.Password_Hash=reader.GetValue(4).ToString();
+                    u.Registered_At=DateTime.Parse( reader.GetValue(5).ToString());
+                    u.Last_Login= DateTime.Parse(reader.GetValue(6).ToString());
+                    u.Intro=reader.GetValue(7).ToString();
+                    u.Description=reader.GetValue(8).ToString();
+                    u.Location=reader.GetValue(8).ToString();
+                    u.Mobile=reader.GetValue(8).ToString();
+                    u.Profile_Image=reader.GetValue(8).ToString();
+
+                    users.Add(u);
+                }
+
+                return users;
+            }
+
+        }
+        [HttpGet]
+        [Route("api/[controller]/{id}")]
+        public List<User> GetUser(string id)
+        {
+            string query = "SELECT * FROM Users WHERE ID = @ID";
+            string connectionString = Configuration.GetConnectionString("PureDatabase");
+            var jsonResult = new StringBuilder();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                command.Parameters.Add("@ID",System.Data.SqlDbType.NChar);
+                command.Parameters["@ID"].Value = id;
+                var reader = command.ExecuteReader();
+                List<User> users = new List<User>();
+                while (reader.Read())
+                {
+                    User u = new User();
+                    u.ID = reader.GetValue(0).ToString();
+                    u.First_Name = reader.GetValue(1).ToString();
+                    u.Last_Name = reader.GetValue(2).ToString();
+                    u.Username = reader.GetValue(3).ToString();
+                    u.Password_Hash = reader.GetValue(4).ToString();
+                    u.Registered_At = DateTime.Parse(reader.GetValue(5).ToString());
+                    u.Last_Login = DateTime.Parse(reader.GetValue(6).ToString());
+                    u.Intro = reader.GetValue(7).ToString();
+                    u.Description = reader.GetValue(8).ToString();
+                    u.Location = reader.GetValue(8).ToString();
+                    u.Mobile = reader.GetValue(8).ToString();
+                    u.Profile_Image = reader.GetValue(8).ToString();
+
+                    users.Add(u);
+                }
+
+                return users;
+            }
+
+
+        }
+    }
+}
---
+++ pureconnect/User.cs
@@ -0,0 +1,30 @@
+﻿namespace pureconnect
+{
+    public class User
+    {
+        public string ID { get; set; }
+
+        public string First_Name { get; set; }
+
+        public string Last_Name { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password_Hash { get; set; }
+
+        public DateTime? Registered_At { get; set; }
+
+        public DateTime? Last_Login { get; set; }
+
+        public string? Intro { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Location { get; set; }
+
+        public string? Mobile { get; set; }
+
+        public string Profile_Image { get; set; }
+
+    }
+}
