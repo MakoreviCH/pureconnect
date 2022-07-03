7a70efd68d5887687a15c76b18bd263e2e105752
MakoreviCH <rostik33@hotmail.com>
2022-07-03 18:39:08 +0300

    delete and update users

---
+++ pureconnect/Models/UserUpdateProfile.cs
@@ -0,0 +1,21 @@
+﻿namespace pureconnect.Models
+{
+	public class UserUpdateProfile
+	{
+		public string ID { get; set; }
+
+		public string First_Name { get; set; }
+
+		public string Last_Name { get; set; }
+
+		public string Username { get; set; }
+
+		public string? Intro { get; set; }
+
+		public string? Description { get; set; }
+
+		public string? Location { get; set; }
+
+		public string Profile_Image { get; set; }
+	}
+}
