579f6f2efe9ed43ea8d3e27f3a315bd17ab9686e
KeeYoo <poronko2045@gmail.com>
2022-07-05 14:46:26 +0300

    Details fixed

--- pureconnect/Controllers/StoreController.cs
+++ pureconnect/Controllers/StoreController.cs
@@ -15,8 +15,8 @@ namespace pureconnect.Controllers
             Configuration = _configuration;
         }
 
-        [HttpGet("storeById")]
-        public Store GetStore(int storeId, string userId)
+        [HttpGet("byId")]
+        public Store GetStore(int Store_ID, string User_ID)
         {
             string query = "SELECT Stores.ID, Stores.User_ID, Stores.Store_Name, Stores.Description, Stores.Location, Stores.Photo, Stores.Background_Image, " +
                 "Stores.Count_Followers, Stores.Count_Sales FROM Stores WHERE ID = @Store_ID";
@@ -28,7 +28,7 @@ namespace pureconnect.Controllers
                 SqlCommand command = new SqlCommand(query, connection);
                 connection.Open();
                 command.Parameters.Add("@Store_ID", System.Data.SqlDbType.Int);
-                command.Parameters["@Store_ID"].Value = storeId;
+                command.Parameters["@Store_ID"].Value = Store_ID;
 
                 var reader = command.ExecuteReader();
                 Store store = new Store();
@@ -44,7 +44,7 @@ namespace pureconnect.Controllers
                     store.Background_Image = reader.GetValue(6).ToString();
                     store.Count_Followers = Convert.ToInt32(reader.GetValue(7));
                     store.Count_Sales = Convert.ToInt32(reader.GetValue(8));
-                    if (IsFollowed(storeId, userId))
+                    if (IsFollowed(Store_ID, User_ID))
                     {
                         store.Is_Followed = true;
                     }
@@ -60,7 +60,7 @@ namespace pureconnect.Controllers
         }
 
         [HttpGet("isFollowed")]
-        public bool IsFollowed(int storeId, string userId)
+        public bool IsFollowed(int Store_ID, string User_ID)
         {
             string query = "SELECT Stores.ID, Stores.User_ID, Stores.Store_Name, Stores.Description, Stores.Location, Stores.Photo, Stores.Background_Image, Stores.Count_Followers, Stores.Count_Sales FROM Stores INNER JOIN Store_Follows ON Stores.ID = Store_Follows.Store_Id WHERE Store_Follows.User_ID = @User_ID AND Store_Follows.Store_ID = @ID";
             string connectionString = Configuration.GetConnectionString("PureDatabase");
@@ -70,10 +70,10 @@ namespace pureconnect.Controllers
                 SqlCommand command = new SqlCommand(query, connection);
                 connection.Open();
                 command.Parameters.Add("@ID", System.Data.SqlDbType.Int);
-                command.Parameters["@ID"].Value = storeId;
+                command.Parameters["@ID"].Value = Store_ID;
 
                 command.Parameters.Add("@User_ID", System.Data.SqlDbType.VarChar);
-                command.Parameters["@User_ID"].Value = userId;
+                command.Parameters["@User_ID"].Value = User_ID;
 
                 var reader = command.ExecuteReader();
                 
@@ -90,7 +90,7 @@ namespace pureconnect.Controllers
         }
 
         [HttpGet("postsByStoreId")]
-        public List<StorePosts> GetPostsByStoreId(int storeId)
+        public List<StorePosts> GetPostsByStoreId(int Store_ID)
         {
             string query = "SELECT ID, Store_ID, Product_Name, Price, Photos FROM Store_Posts WHERE Store_ID = @Store_ID";
             string connectionString = Configuration.GetConnectionString("PureDatabase");
@@ -101,7 +101,7 @@ namespace pureconnect.Controllers
                 SqlCommand command = new SqlCommand(query, connection);
                 connection.Open();
                 command.Parameters.Add("@Store_ID", System.Data.SqlDbType.Int);
-                command.Parameters["@Store_ID"].Value = storeId;
+                command.Parameters["@Store_ID"].Value = Store_ID;
 
                 var reader = command.ExecuteReader();
                 while (reader.Read())
@@ -164,7 +164,7 @@ namespace pureconnect.Controllers
 
         }
 
-        [HttpPut("updateStore")]
+        [HttpPut("update")]
         public ActionResult UpdatePost([FromBody] StoreUpdate s)
         {
             string query = "UPDATE Store SET Photo = @Photo, Store_Name = @Store_Name, Description = @Description, Location = @Location, Background_Image = @Background_Image WHERE ID = @ID";
@@ -207,7 +207,7 @@ namespace pureconnect.Controllers
         }
 
         [HttpGet("followedStoresByUser")]
-        public List<StoreForSearch> GetFollowedStoresByUserId(string userId)
+        public List<StoreForSearch> GetFollowedStoresByUserId(string User_Id)
         {
             string query = "SELECT Store_Follows.Store_ID, Stores.Store_Name, Stores.Description, Stores.Photo FROM Stores INNER JOIN Store_Follows ON Stores.ID = Store_Follows.Store_ID WHERE Store_Follows.User_ID = @User_ID";
 
@@ -218,7 +218,7 @@ namespace pureconnect.Controllers
                 SqlCommand command = new SqlCommand(query, connection);
                 connection.Open();
                 command.Parameters.Add("@User_ID", System.Data.SqlDbType.NVarChar);
-                command.Parameters["@User_ID"].Value = userId;
+                command.Parameters["@User_ID"].Value = User_Id;
 
                 var reader = command.ExecuteReader();
                 StoreForSearch storeForSearch = new StoreForSearch();
@@ -236,6 +236,74 @@ namespace pureconnect.Controllers
                 return list;
 
             }
+        }
+
+        [HttpGet("searchByName")]
+        public List<SearchStoreToUser> GetStoresByName(string Name, string User_Id)
+        {
+            string query = "SELECT ID, Store_Name, Description, Photo FROM Stores WHERE Store_Name LIKE @Store_Name + '%'";
+
+            string connectionString = Configuration.GetConnectionString("PureDatabase");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+
+                command.Parameters.Add("@Store_Name", System.Data.SqlDbType.NVarChar);
+                command.Parameters["@Store_Name"].Value = Name;
+
+                var reader = command.ExecuteReader();
+                List<SearchStoreToUser> list = new List<SearchStoreToUser>();
+                while (reader.Read())
+                {
+                    SearchStoreToUser storeForSearch = new SearchStoreToUser();
+                    storeForSearch.Id = Convert.ToInt32(reader.GetValue(0));
+                    storeForSearch.Store_Name = reader.GetValue(1).ToString();
+                    storeForSearch.Description = reader.GetValue(2).ToString();
+                    storeForSearch.Photo = reader.GetValue(3).ToString();
+
+                    if (IsFollowed(storeForSearch.Id, User_Id))
+                    {
+                        storeForSearch.IsFollowerd = true;
+                    }
+                    else
+                    {
+                        storeForSearch.IsFollowerd = false;
+                    }
+
+                    list.Add(storeForSearch);
+                }
+
+                return list;
+
+            }
+
+        }
+        [HttpDelete("delete")]
+        public ActionResult DeleteStore(int Id)
+        {
+            string query = "DELETE FROM Stores WHERE ID = @ID";
+            string connectionString = Configuration.GetConnectionString("PureDatabase");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                connection.Open();
+                command.Parameters.Add("@ID", System.Data.SqlDbType.Int);
+                command.Parameters["@ID"].Value = Id;
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch
+                {
+                    return new StatusCodeResult(204);
+                }
+            }
+            return new StatusCodeResult(200);
+
         }
     }
 }
---
+++ pureconnect/Models/SearchStoreToUser.cs
@@ -0,0 +1,11 @@
+﻿namespace pureconnect.Models
+{
+    public class SearchStoreToUser
+    {
+        public int Id { get; set; }
+        public string Store_Name { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+        public bool IsFollowerd { get; set; }
+    }
+}
